426fc63b2b761b0d0685a61924ced21b17c80d22
Michal Biros <michal.biros@itera.no>
2019-01-25 15:02:43 +0100

    Setup server rendering only for bots

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import { renderToString } from 'react-dom/server';
 import React from 'react';
 import App from './src/App';
 import { StaticRouter as Router } from 'react-router-dom';
+import amibot from 'amibot';
 
 const app = express();
 const port = 8088;
@@ -21,7 +22,7 @@ const readFile = (path, opts = 'utf8') => {
 };
 
 app.use(express.static('build'));
-
+let browser = null;
 /*
  * This solution si using React.renderToString.
  */
@@ -47,10 +48,13 @@ app.get('/star-wars-preloaded/4', async (req, res) => {
  * It renders the whole application, waits until it loads
  * everything it needs and send the html back to the client.
  */
-app.get('/star-wars/:id', async (req, res) => {
+async function getPrerenderedPage(req, res) {
   const data = await readFile('build/index.html', 'utf8');
 
-  const browser = req.app.get('browser');
+  if (! browser ) {
+    browser = await puppeteer.launch({ headless: true });
+  }
+
   const page = await browser.newPage();
   await page.setRequestInterception(true);
   /*
@@ -60,7 +64,20 @@ app.get('/star-wars/:id', async (req, res) => {
    * changed.
    */
   page.on('request', request => {
-    if (request.url() === `http://localhost:${port}/star-wars/${req.params.id}`) {
+    // Don't load Google Analytics lib requests so pageviews aren't 2x.
+    const blacklist = ['www.google-analytics.com', '/gtag/js', 'ga.js', 'analytics.js'];
+    if (blacklist.find(regex => request.url().match(regex))) {
+      return request.abort();
+    }
+
+    // Ignore requests for resources that don't produce DOM
+    // (images, stylesheets, media).
+    // const whitelist = ['document', 'script', 'xhr', 'fetch'];
+    // if (!whitelist.includes(request.resourceType())) {
+    //   return request.abort();
+    // }
+
+    if (request.url().startsWith(`http://localhost:${port}`) && request.resourceType() === 'document') {
       request.respond({
         status: 200,
         contentType: 'text/html',
@@ -76,7 +93,7 @@ app.get('/star-wars/:id', async (req, res) => {
    * Therefore we need ot let the application know, which URL
    * we want it to render.
    */
-  await page.goto(`http://localhost:${port}/star-wars/${req.params.id}`, { waitUntil: 'networkidle2' });
+  await page.goto(`http://localhost:${port}${req.originalUrl}`, { waitUntil: 'networkidle2' });
   /*
    * In the previous call we wait for React app to download
    * everything it needs, but from time to time, you can
@@ -86,17 +103,21 @@ app.get('/star-wars/:id', async (req, res) => {
    * immediately, than there is a possibility it will not be
    * rendered yet.
    */
-  await page.waitFor(100);
+  await page.waitFor(500);
   const content = await page.content();
 
   page.close();
 
   res.write(content);
   res.end();
-});
+}
 
-app.get('*', (req, res) => {
-  res.sendFile('./build/index.html', {root: './'});
+app.get('*', async (req, res) => {
+  if (amibot(req.headers['user-agent'])) {
+    return await getPrerenderedPage(req, res);
+  } else {
+    res.sendFile('./build/index.html', {root: './'});
+  }
 });
 
 async function start() {
@@ -104,7 +125,6 @@ async function start() {
    * Store browser in express app, so that we do not need
    * to start it again every time.
    */
-  app.set('browser', await puppeteer.launch({ headless: true }));
   app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 }
 
